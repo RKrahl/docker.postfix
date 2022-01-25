6a6d3d52ce3f8485c0fa8e0d14b9f23e3ba2aab8
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2022-01-25 16:35:53 +0100

    Update base image to opensuse 15.3

--- start-postfix.sh
+++ start-postfix.sh
@@ -4,11 +4,11 @@
 
 test -f /etc/sysconfig/postfix && . /etc/sysconfig/postfix
 
-/etc/postfix/system/config_postfix
-/etc/postfix/system/update_chroot
-/etc/postfix/system/update_postmaps
+/usr/lib/postfix/systemd/config_postfix
+/usr/lib/postfix/systemd/update_chroot
+/usr/lib/postfix/systemd/update_postmaps
 
 /usr/sbin/postfix start
 
-/etc/postfix/system/wait_qmgr 60
-/etc/postfix/system/cond_slp register
+/usr/lib/postfix/systemd/wait_qmgr 60
+/usr/lib/postfix/systemd/cond_slp register
